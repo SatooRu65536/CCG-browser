94fec62d77aef3b7096b8c67fa9fc2e7f14d95fa
Nagi65536 <87361015+Nagi65536@users.noreply.github.com>
2022-05-13 13:51:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const color_space   = docment.getElementID('color-space')
+const color_code    = docment.getElementID('color-code')
+const canswer       = docment.getElementID('canswer')
+const canswer_code  = docment.getElementID('canswer-code')
+const canswer_color = docment.getElementID('canswer-color')
+const answer        = docment.getElementID('answer')
+const answer_code   = docment.getElementID('answer-code')
+const answer_color  = docment.getElementID('answer-color')
+const err           = docment.getElementID('err')
+const point         = docment.getElementID('point')
 
+function generate() {
+  let rgb10 = []
+  let rgb16 = []
+  for(let i=0; i<3; i++){
+    rgb10[i] = Math.floor(Math.random() * 256)
+    rgb16[i] = rgb10.toString(16)
+  }
+  color_space.style.backgroundColor = ‘#${rgb16.join('')}’
+}
+
+function check() {
+  
+}
