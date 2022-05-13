5ff0e73fcb5ebb79959d157a29fff4740a8369c6
nagi <ryuto65536@icloud.com>
2022-05-13 21:54:58 +0900

    最低限

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-const color_space   = docment.getElementID('color-space')
-const color_code    = docment.getElementID('color-code')
-const canswer       = docment.getElementID('canswer')
-const canswer_code  = docment.getElementID('canswer-code')
-const canswer_color = docment.getElementID('canswer-color')
-const answer        = docment.getElementID('answer')
-const answer_code   = docment.getElementID('answer-code')
-const answer_color  = docment.getElementID('answer-color')
-const err           = docment.getElementID('err')
-const point         = docment.getElementID('point')
+const color_space = document.getElementById('color-space');
+const color_code = document.getElementById('color-code');
+const canswer = document.getElementById('canswer');
+const canswer_code = document.getElementById('canswer-code');
+const canswer_color = document.getElementById('canswer-color');
+const answer = document.getElementById('answer');
+const answer_code = document.getElementById('answer-code');
+const answer_color = document.getElementById('answer-color');
+
+const res_err = document.getElementById('res-err');
+const res_r = document.getElementById('res-r');
+const res_g = document.getElementById('res-g');
+const res_b = document.getElementById('res-b');
 
 generate()
 
 function generate() {
-  let rgb10 = []
-  let rgb16 = []
-  for(let i=0; i<3; i++){
-    rgb10[i] = Math.floor(Math.random() * 256)
-    rgb16[i] = rgb10.toString(16)
+  rgb10 = []
+  rgb16 = []
+  for (let i = 0; i < 3; i++) {
+    rgb10[i] = Math.floor(Math.random() * 256);
+    rgb16[i] = rgb10[i].toString(16);
   }
-  color_space.style.backgroundColor = ‘#${rgb16.join('')}’
-  color_space.innerText = ‘#${rgb16.join('')}’
+  color_space.style.backgroundColor = `#${rgb16.join('')}`;
 }
 
 function check() {
-  
+  input_value = color_code.value.toLowerCase();
+  console.log(input_value);
+
+  let input_rgb10 = []
+  let input_rgb16 = []
+  let point = 0;
+
+  for (let i = 0; i < 3; i++) {
+    input_rgb10[i] = parseInt(input_value.substring(2 * i + 1, 2 * i + 3), 16);
+    input_rgb16[i] = parseInt(input_rgb10[i], 16);
+    point += Math.abs(input_rgb10[i] - rgb10[i])
+  }
+
+  if (isNaN(point)) {
+    res_err.innerText = '入力が間違っています';
+    res_r.innerText = '';
+    res_g.innerText = '';
+    res_b.innerText = '';
+  } else {
+    res_err.innerText = point;
+    res_r.innerText = rgb10[0] - input_rgb10[0];
+    res_g.innerText = rgb10[1] - input_rgb10[1];
+    res_b.innerText = rgb10[2] - input_rgb10[2];
+  }
 }
