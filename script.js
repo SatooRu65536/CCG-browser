57e82ad10bd47b6578a36c1bb86347deb07046a0
nagi <ryuto65536@icloud.com>
2022-05-14 11:50:23 +0900

    0埋めされてない問題解決

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const res_b = document.getElementById('res-b');
 
 generate()
 
+function zeroPadding(NUM, LEN){
+	return ( Array(LEN).join('0') + NUM ).slice( -LEN );
+}
+
 function generate() {
   rgb10 = []
   rgb16 = []
@@ -21,12 +25,17 @@ function generate() {
     rgb10[i] = Math.floor(Math.random() * 256);
     rgb16[i] = rgb10[i].toString(16);
   }
-  color_space.style.backgroundColor = `#${rgb16.join('')}`;
+  canswer_color_code = '';
+
+  for (let i = 0; i < rgb16.length; i++) {
+    canswer_color_code += zeroPadding(rgb16[i], 2);
+  }
+
+  color_space.style.backgroundColor = `#${canswer_color_code}`;
 }
 
 function check() {
   input_value = color_code.value.toLowerCase();
-  console.log(input_value);
 
   let input_rgb10 = []
   let input_rgb16 = []
@@ -34,7 +43,7 @@ function check() {
 
   for (let i = 0; i < 3; i++) {
     input_rgb10[i] = parseInt(input_value.substring(2 * i, 2 * i + 2), 16);
-    input_rgb16[i] = parseInt(input_rgb10[i], 16);
+    input_rgb16[i] = input_rgb10[i].toString(16)
     point += Math.abs(input_rgb10[i] - rgb10[i])
   }
 
@@ -43,10 +52,26 @@ function check() {
     res_r.innerText = '';
     res_g.innerText = '';
     res_b.innerText = '';
+
+    answer_code.innerText = '';
+    canswer_code.innerText = '';
+    answer_color.style.backgroundColor = '#fff';
+    canswer_color.style.backgroundColor = '#fff';
   } else {
     res_err.innerText = point;
     res_r.innerText = rgb10[0] - input_rgb10[0];
     res_g.innerText = rgb10[1] - input_rgb10[1];
     res_b.innerText = rgb10[2] - input_rgb10[2];
+
+    answer_color_code = '';
+    for (let i = 0; i < rgb16.length; i++) {
+      answer_color_code += zeroPadding(input_rgb16[i], 2);
+    }
+
+    answer_code.innerText = `#${answer_color_code}`;
+    canswer_code.innerText = `#${canswer_color_code}`;
+    answer_color.style.backgroundColor = `#${answer_color_code}`;
+    canswer_color.style.backgroundColor = `#${canswer_color_code}`;
+    console.log(rgb16);
   }
 }
